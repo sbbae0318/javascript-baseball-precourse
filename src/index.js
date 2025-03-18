fbeff6526d0cb9efe76c17cb53b12dd0fd6657a6
sbbae0318 <sbbae0318@gmail.com>
2025-03-19 06:24:17 +0900

    [feat] button event listeners
    DomContentLoaded
    submit
    click
    onClick

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 export default function BaseballGame() {
+    this.answer = 0;
     this.init = function() {
         let numbers = Array.from({ length: 9 }, (_, i) => i + 1)
 
         let idx = MissionUtils.Random.pickNumberInRange(0, 8)
-        let a = numbers.splice(idx, 1)
+        let a = numbers.splice(idx, 1)[0]
 
         idx = MissionUtils.Random.pickNumberInRange(0, 7)
-        let b = numbers.splice(idx, 1)
+        let b = numbers.splice(idx, 1)[0]
 
         idx = MissionUtils.Random.pickNumberInRange(0, 6)
-        let c = numbers.splice(idx, 1)
+        let c = numbers.splice(idx, 1)[0]
 
-        console.log(`${a} ${b} ${c}`)
-        return 100 * a + 10 * b + c;
+        this.answer = 100 * a + 10 * b + c;
+        console.log(`${a} ${b} ${c} ${this.answer}`)
+        return this.answer;
     }
 
     this.getMessage = function (strike, ball) {
@@ -27,8 +29,8 @@ export default function BaseballGame() {
     }
 
     this.sanityCheck = function (userInputNumbers) {
-        const numStr = userInputNumbers.toString();
-        return new Set(numStr).size === numStr.length;
+        let charArray = [...userInputNumbers.toString()]
+        return new Set(charArray).size === charArray.length;
     }
 
     this.play = function (computerInputNumbers, userInputNumbers) {
@@ -53,34 +55,56 @@ export default function BaseballGame() {
 
         return this.getMessage(strike, ball)
     };
-
-    /*
-    this.badSanityCheck = function (userInputNumbers) {
-        userInputNumbers = userInputNumbers.toString()
-        let charArray = [...userInputNumbers];
-        let set = new Set()
-
-        for (let char of charArray) {
-            if (set.has(char))
-                return false
-            set.add(char)
-        }
-
-        return true
-    }*/
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-        const game = new BaseballGame();
-        game.init()
-        game.play(123, 456); // '낫싱'
-        game.play(123, 345); // '1볼'
-        game.play(123, 432); // '2볼'
-        game.play(123, 312); // '3볼'
-        game.play(123, 145); // '1스트라이크'
-        game.play(123, 134); // '1볼 1스트라이크'
-        game.play(123, 132); // '2볼 1스트라이크'
-        game.play(123, 124); // '2스트라이크'
+const input = document.getElementById('user-input')
+const result = document.getElementById('result')
+const restart = document.getElementById('game-restart-button')
+
+let game
+
+function startGame() {
+    game = new BaseballGame();
+    game.init();
+
+    input.value = '';
+    result.textContent = '';
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    startGame()
+
+    const app = document.getElementById('app')
+    //const submit = document.getElementById('submit')
+    // submit.onClick과의 차이점 기억하기.
+
+    app.addEventListener('submit', (e) => {
+        e.preventDefault()
+        console.log('submit', e)
+        console.log(input.value)
+        const userInputNumbers = parseInt(input.value, 10);
+
+        if (!game.sanityCheck(userInputNumbers)) {
+            result.textContent = "잘못 입력함."
+            return
+        }
+
+        result.textContent = game.play(game.answer, userInputNumbers)
+        input.value = '';
+    })
 });
+
+restart.addEventListener('click', startGame);
+
+    //game.sanityCheck(999)
+    //game.sanityCheck(123)
+    //game.play(123, 456); // '낫싱'
+    //game.play(123, 345); // '1볼'
+    //game.play(123, 432); // '2볼'
+    //game.play(123, 312); // '3볼'
+    //game.play(123, 145); // '1스트라이크'
+    //game.play(123, 134); // '1볼 1스트라이크'
+    //game.play(123, 132); // '2볼 1스트라이크'
+    //game.play(123, 124); // '2스트라이크'
 
 
